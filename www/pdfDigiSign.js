73482bea8a768ea1561a72e40422a7e98aaadb51
Erik Paixão <erik.reis@maisvida.med.br>
2018-03-13 11:40:23 -0300

    changes in js

--- www/pdfDigiSign.js
+++ www/pdfDigiSign.js
@@ -1,6 +1,6 @@
 var exec = require('cordova/exec');
 
-var PLUGIN_NAME = PDFDigiSign;
+var PLUGIN_NAME = "PDFDigiSign";
 
 var PDFDigiSign = {
     signWithAlias: function (path, alias, name, loc, reason, imageData, page, x, y, width, height, successCallback, errorCallback) {
